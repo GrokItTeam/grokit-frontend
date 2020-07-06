6be0dfdbf831febd5ebaeea70cda2a5957361208
Saoirse Bryar <smbryar@gmail.com>
2020-07-06 11:58:12 +0100

    Add timer page and route, push there from button

--- src/components/SkillsToDo/SkillToDoItem/Skill.js
+++ src/components/SkillsToDo/SkillToDoItem/Skill.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Button } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
 import MarkSkillAsPractised from './MarkSkillAsPractised';
 
 
-function Skill({ projectName = "", skill={}, updatePractisedSkill = () => { } }) {
+function Skill({ history, projectName = "", skill={}, updatePractisedSkill = () => { } }) {
 
     return (
         <Row>
             <p>{projectName}: {skill.name}</p>
+            <Button onClick = {() => history.push("/timer")}>Timer</Button>
             <MarkSkillAsPractised updatePractisedSkill = {updatePractisedSkill} skill={skill}/>
         </Row>
     )
 }
 
-export default Skill;
+export default withRouter(Skill);
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Timer() {
+
+    return (
+        <p>Timer</p>
+    )
+}
+
+export default Timer;
