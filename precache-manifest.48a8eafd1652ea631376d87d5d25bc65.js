ab2d83439d507f18479c138a6f55d81ba00e1200
Saoirse Bryar <smbryar@gmail.com>
2020-07-07 11:54:04 +0100

    Updates

---
+++ precache-manifest.48a8eafd1652ea631376d87d5d25bc65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e287ff5b46ded40ea03403776ab3c2ee",
+    "url": "/grokit-frontend/index.html"
+  },
+  {
+    "revision": "8e3b1103a9bf0fd2e9db",
+    "url": "/grokit-frontend/static/css/main.7de66c90.chunk.css"
+  },
+  {
+    "revision": "75619db4493dcedcead6",
+    "url": "/grokit-frontend/static/js/2.8e041e8f.chunk.js"
+  },
+  {
+    "revision": "a200174c44a1c583da2ca47c7148e922",
+    "url": "/grokit-frontend/static/js/2.8e041e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3b1103a9bf0fd2e9db",
+    "url": "/grokit-frontend/static/js/main.3c81c454.chunk.js"
+  },
+  {
+    "revision": "a922b33d079b6e8f794b",
+    "url": "/grokit-frontend/static/js/runtime-main.028a27d6.js"
+  }
+]);
