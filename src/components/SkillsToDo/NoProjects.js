623f192ab0bea4e521c1c78b13fd5604f00e992d
ruzina75@gmail.com <ruzina75@gmail.com>
2020-07-07 19:32:30 +0100

    colour changes on navbar, text  for registration

--- src/components/SkillsToDo/NoProjects.js
+++ src/components/SkillsToDo/NoProjects.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import {Row, Col} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import {timesTablesProject, javascriptProject, footballProject} from 'components/ProjectList/ExampleProjects/exampleProjectData';
-import ExampleProject from 'components/ProjectList/ExampleProjects/ExampleProject';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { timesTablesProject, javascriptProject, footballProject } from "components/ProjectList/ExampleProjects/exampleProjectData";
+import ExampleProject from "components/ProjectList/ExampleProjects/ExampleProject";
 
-function NoProjects({ projects = [], open = true, updatePractisedSkill = () => { }, addProject = () => {} }) {
-
-    return (
-        <>
-            <div>
-                Welcome to grokit. Let's get started by adding some projects that you want to grok. <br />
-          Look at the the example projects below and then click <Link to="/projects">here</Link> to start adding your own.
-        </div>
-        <Row>
-            <Col>
-            <ExampleProject addProject={addProject} projects = {projects} project = {timesTablesProject} />
-            </Col>
-            <Col>
-            <ExampleProject addProject={addProject} projects = {projects} project = {javascriptProject} />
-            </Col>
-            <Col>
-            <ExampleProject addProject={addProject} projects = {projects} project = {footballProject} />
-            </Col>
-        </Row>
-        </>
-
-    );
+function NoProjects({ projects = [], open = true, updatePractisedSkill = () => {}, addProject = () => {} }) {
+  return (
+    <>
+      <div className="NoProjects__description">
+        Welcome to Grokit. Let's get started by adding some projects that you want to Grok. <br />
+        Look at the the example projects below and then click <Link to="/projects">here</Link> to start adding your own.
+      </div>
+      <Row className="grokitCard">
+        <Col className="grokitCard">
+          <ExampleProject addProject={addProject} projects={projects} project={timesTablesProject} />
+        </Col>
+        <Col>
+          <ExampleProject addProject={addProject} projects={projects} project={javascriptProject} />
+        </Col>
+        <Col>
+          <ExampleProject addProject={addProject} projects={projects} project={footballProject} />
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default NoProjects;
