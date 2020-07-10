ff0bcd1f618b113bfe0199ace163efccdba85818
Saoirse Bryar <smbryar@gmail.com>
2020-07-10 15:43:09 +0100

    Remove console logs

--- src/App.js
+++ src/App.js
@@ -179,7 +179,6 @@ function App() {
   };
 
   const editSkillName = (skillId, skillName) => {
-    console.log("making request");
     axios
       .put(
         `https://zlld6v728l.execute-api.eu-west-2.amazonaws.com/dev/skills/${skillId}`,
